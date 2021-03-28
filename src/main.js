187fe4ec468e87c5553d04befaa3ac57e1646efd
marlonxteban <marlonxteban@gmail.com>
2021-03-28 01:53:20 -0500

    fix: changes for heroku deploy.

--- src/main.js
+++ src/main.js
@@ -3,18 +3,18 @@ import App from '@/app';
 import router from './router';
 
 // Import the Auth0 configuration and plugin
-import { domain, clientId, audience } from '../auth_config.json';
+import { domain as localDomain, clientId as localClientId, audience as localAudience} from '../auth_config.json';
 import { Auth0Plugin } from '@/auth/auth0-plugin';
 
-const actualDomain = process.env.DOMAIN || domain;
-const actualClientId = process.env.CLIENTID || clientId;
-const actualAudience = process.env.AUDIENCE || audience;
+const domain = process.env.DOMAIN || localDomain;
+const clientId = process.env.CLIENTID || localClientId;
+const audience = process.env.AUDIENCE || localAudience;
 
 // Install the authentication plugin
 Vue.use(Auth0Plugin, {
-  actualDomain,
-  actualClientId,
-  actualAudience,
+  domain,
+  clientId,
+  audience,
   onRedirectCallback: appState => {
     router.push(
       appState && appState.targetUrl
